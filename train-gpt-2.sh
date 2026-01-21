c6a2e1679b08a5bf20ba451abbaf9bf4cad1df79
typename-yyf <85046538+typename-yyf@users.noreply.github.com>
2026-01-21 21:53:52 +0800

    Update train-gpt-2.sh

--- train-gpt-2.sh
+++ train-gpt-2.sh
@@ -1,52 +1,42 @@
-
-LOCAL_BATCH_SIZE=1
-GRAD_ACC=40
-NPROC=4
-# total batchsize = LOCAL_BATCH_SIZE * GRAD_ACC * NPROC
+LOCAL_BATCH_SIZE=64
+GRAD_ACC=8
+NPROC=1
 
 TAG=your-tag
-PORT=12345
+PORT=13222
 
-python -m torch.distributed.launch --nproc_per_node $NPROC --master-port $PORT dp_main.py \
-    --chkpt-dir /your/checkpoint/dir \
+nohup python dp_main.py > /inspire/hdd/project/yunweiyuhuifu/p-shangli/cmy/Metis-Hif4/nohupout/$TAG.log \
+    --chkpt-dir /your/checkpoint/path \
     --dataset-path ./dataset \
-    --log-dir /your/log/dir \
+    --log-dir /your/log/path \
     --tokenizer-path /your/tokenizer/path \
+    --device 5 \
     --tag $TAG \
     --reg-lambda 0 \
     --layers 12 \
     --embed-dim 768 \
-    --max-epochs 3 \
+    --max-epochs 4 \
     --heads 12 \
-    --n-kv-heads 12 \
-    --q-forward-input fp4e2m1b \
-    --q-forward-weight fp4e2m1b \
-    --q-backward-input fp4e2m1b \
-    --q-backward-weight fp4e2m1b \
-    --q-backward-outputgrad fp4e2m1b \
-    --q-scalar 1.0 \
+    --lr-warmup-steps 50 \
     --grad-clipping 2.0 \
     --win-size 256 \
     --forward-svd-warmup-steps 0 \
     --forward-svd-merge-steps -1 \
     --batch-size $LOCAL_BATCH_SIZE \
-    --weight-decay 0.1 \
-    --lr 1.5e-4 \
-    --merged-lr 1.5e-4 \
+    --lr 1e-4 \
+    --merged-lr 1e-4 \
     --grad-acc $GRAD_ACC \
-    --lr-warmup-steps 20 \
+    --train-steps 400000 \
+    --q-forward-input nvfp4e2m1bnosr \
+    --q-forward-weight nvfp4e2m1bnosr \
+    --q-backward-input nvfp4e2m1bnosr \
+    --q-backward-weight nvfp4e2m1bnosr \
+    --q-backward-outputgrad nvfp4e2m1b \
     --enable-lowbit \
+    --save-steps 10000 \
+    --enable-forward-svd-intime \
+    --forward-lowrank-svd-intime 16 \
     --enable-backward-svd \
-    --backward-lowrank-svd 60 \
-    --backward-lowrank-niter 0 \
-
-
-
-
-
-
-
-    
-    
-    
-    
+    --backward-lowrank-svd 16 \
+    --backward-lowrank-niter 2 \
+    --backward-broadcast-dim -1 \
